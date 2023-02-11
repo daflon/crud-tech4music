7fbe1f47210c7262e5504e30300f8cc6703ddcb0
daflon <cdaflon@gmail.com>
2023-02-11 12:53:52 -0300

    Entregando a prova

--- src/main/java/br/com/tech4me/crudtech4music/view/model/MusicaRequest.java
+++ src/main/java/br/com/tech4me/crudtech4music/view/model/MusicaRequest.java
@@ -1,18 +1,31 @@
 package br.com.tech4me.crudtech4music.view.model;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 public class MusicaRequest {
 
+    @NotBlank(message = "O nome do artista não pode ter caracteres em branco")
+    @NotEmpty(message = "O nome do artista não pode ser vazio")
     private String artista;
+    
     @NotBlank(message = "O nome do album não pode ter caracteres em branco")
     @NotEmpty(message = "O nome do album não pode ser vazio")
-    @Size(min = 5, max= 10, message = "O nome do album deve ter no minimo 5 e no maximo 10 caracteres")
     private String album;
+    
+    @NotBlank(message = "O gênero não pode ter caracteres em branco")
+    @NotEmpty(message = "O gênero não pode ser vazio")
     private String genero;
+    
+    @Digits (fraction = 0, integer = 4, message = "O ano de lançamento deve conter 4 caracteres conforme ex: '1982'")
     private int anoLancamento;
+    
+    @NotBlank(message = "O nome do compositor não pode ter caracteres em branco")
+    @NotEmpty(message = "O nome do compositor não pode ser vazio")
     private String compositor;
 
     public String getArtista() {
